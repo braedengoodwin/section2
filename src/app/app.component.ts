540e426e0fee3e404a6ce56c50899afab874afe4
braedengoodwin <braedenagoodwin@hotmail.com>
2025-09-17 16:14:05 -0500

    added more files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComonent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComonent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
