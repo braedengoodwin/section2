316e4648df37731794b53f79506cb753b5e627c4
braedengoodwin <braedenagoodwin@hotmail.com>
2025-09-18 15:49:45 -0500

    more lessons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HeaderComonent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComonent],
+  imports: [HeaderComonent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  user = DUMMY_USERS;
+
+}
