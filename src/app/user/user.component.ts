038ebe5760caa45973507bf917cfe1a19f242f0a
braedengoodwin <braedenagoodwin@hotmail.com>
2025-09-23 16:34:50 -0500

    message

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -17,6 +10,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
